cd3c605647d25626cb05d2df59e5905ae7a9f200
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-06-16 14:53:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown (n)
+  while n > 0 
+    puts "#{n} SECOND(S)!"
+  n -= 1 
+  countdown_with_sleep(1)
+  end
+    countdown_with_sleep(5)
+    puts "HAPPY NEW YEAR!"
 end
